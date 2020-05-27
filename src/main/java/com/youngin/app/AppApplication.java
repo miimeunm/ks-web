1a7c71bfc97b73ea8efc31abab5239164c21337f
miimuemn <rhranfe@gmail.com>
2020-05-27 20:20:27 +0900

    2nd commit

--- src/main/java/com/youngin/app/AppApplication.java
+++ src/main/java/com/youngin/app/AppApplication.java
@@ -30,7 +30,7 @@ public class AppApplication implements CommandLineRunner {
 	@Override
 	public void run(String... args) throws Exception {
 		personRepository.save(new Person("구영인", "데이터 분석", "hello@test.com", "010-1234-5678"));
-		socialMediaRepository.save(new SocialMedia("anonymous", "anonymous"));
+		socialMediaRepository.save(new SocialMedia("anonymous", "anonymous" ,"",""));
 		interestsRepository.save(new Interests("음악듣기, 게임하기, 유튜브 영상시청"));
 		skillRepository.save(new Skill("웹프로그래밍응용", 30));
 		workRepository.save(new Work("Student","KSU","2020","더미 데이터를 뭐라고 적어야 할 지 몰라서 학생으로 적었습니다."));
--- src/main/java/com/youngin/app/entity/SocialMedia.java
+++ src/main/java/com/youngin/app/entity/SocialMedia.java
@@ -12,14 +12,18 @@ public class SocialMedia {
     private Long id;
     private String github;
     private String twitter;
+    private String instagram;
+    private String facebook;
 
     public SocialMedia(){
 
     }
 
-    public SocialMedia(String github, String twitter){
+    public SocialMedia(String github, String twitter, String instagram, String facebook) {
         this.github = github;
         this.twitter = twitter;
+        this.instagram = instagram;
+        this.facebook = facebook;
     }
 
     public Long getId() {
@@ -46,4 +50,19 @@ public class SocialMedia {
         this.twitter = twitter;
     }
 
+    public String getInstagram() {
+        return instagram;
+    }
+
+    public void setInstagram(String instagram) {
+        this.instagram = instagram;
+    }
+
+    public String getFacebook() {
+        return facebook;
+    }
+
+    public void setFacebook(String facebook) {
+        this.facebook = facebook;
+    }
 }
